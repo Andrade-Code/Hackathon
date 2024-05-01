a3194ae8ccbb0c2b708e64c87651368ab6a7ba0d
Anderson Andrade <andradejesusejjaa@gmail.com>
2024-05-01 14:24:43 -0300

    Atualização geral
    1. inserindo comentários que faltava em partes do código.
    2. atualizando imagens
    3. renomeando arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Configura o calendário com a visualização mensal
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         selectable: true
     });
+
+    // Renderiza o calendário
     calendar.render();
+
+    // Após a renderização, modifica os estilos dos cabeçalhos dos dias da semana
+    const dayHeaders = document.querySelectorAll('.fc-day-header');
+    dayHeaders.forEach(header => {
+        header.style.backgroundColor = '#f0f0f0';
+        header.style.color = '#333333';
+        header.style.textAlign = 'center';
+        header.style.fontWeight = 'bold';
+    });
 });
 
 // Função para exibir a tabela de acordo com a seleção do módulo
@@ -24,3 +34,33 @@ function mostrarTabela(ele) {
         table2.style.display = 'none';
     }
 }
+
+// Função para aumentar o tamanho da fonte
+function aumentarFonte() {
+    const elements = document.querySelectorAll('body *');
+    elements.forEach(element => {
+        const currentFontSize = parseFloat(window.getComputedStyle(element).fontSize);
+        element.style.fontSize = (currentFontSize + 1) + 'px';
+    });
+}
+
+// Função para diminuir o tamanho da fonte
+function diminuirFonte() {
+    const elements = document.querySelectorAll('body *');
+    elements.forEach(element => {
+        const currentFontSize = parseFloat(window.getComputedStyle(element).fontSize);
+        if (currentFontSize > 10) {  // Define um tamanho mínimo para a fonte
+            element.style.fontSize = (currentFontSize - 1) + 'px';
+        }
+    });
+}
+
+// Função para alternar o contraste (tema claro ou escuro)
+function alternarContraste() {
+    document.body.classList.toggle('contraste-escuro');
+}
+
+// Adicionando eventos de clique aos botões
+document.getElementById('aumentar-fonte').addEventListener('click', aumentarFonte);
+document.getElementById('diminuir-fonte').addEventListener('click', diminuirFonte);
+document.getElementById('alternar-contraste').addEventListener('click', alternarContraste);
